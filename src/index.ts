f9e6901a4f6d883f203d7f07218866d85b0b21c1
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-08 15:59:03 +0100

    Update index.ts
    Error handling in addVehicle and updateVehicle:
    
    Both addVehicle and updateVehicle functions return a Result<Vehicle, Error>, indicating the successful addition or update of a vehicle or an error if it fails. However, the Err object is not properly populated in both cases.
    
    In addVehicle, the Err object should contain a InvalidPayload error message instead of NotFound. This is because the addVehicle function does not check for the existence of a vehicle with the same ID before inserting a new one. If there's already a vehicle with the same ID, it will be overwritten without raising an error.
    
    In updateVehicle, the Err object should also include the original vehicle object instead of just a message. This would provide more context for the error and help identify the specific vehicle that caused the update to fail.
    
    Returning None from getVehicleOpt:
    
    The getVehicle function uses the get method of vehiclesStorage to retrieve a vehicle by ID. The get method returns an Opt(Vehicle), which can either be Some(vehicle) or None.
    
    In the if statement checking for the presence of the vehicle, the if ("None" in vehicleOpt) condition is not necessary. Instead, the more concise if (!vehicleOpt.isSome()) can be used to check if the vehicleOpt is None.
    
    Using createdAt in updateVehicle:
    
    The updateVehicle function updates the updatedAt field of the vehicle to the current time using ic.time(). However, it also attempts to update the createdAt field, which is the initial timestamp when the vehicle was created.
    
    This is not necessary since the createdAt field should only be updated once when the vehicle is initially created. Resetting it during an update could lead to inconsistencies in the timestamps.
    
    Handling None in deleteVehicle:
    
    The deleteVehicle function removes a vehicle from the storage using its ID. It uses the remove method of vehiclesStorage to achieve this, which returns an Opt(Vehicle).
    
    The if ("None" in deletedVehicle) condition is not necessary here either. The more concise if (!deletedVehicle.isSome()) can be used to check if the deletedVehicle is None, indicating that the vehicle was not found.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { query, update, Canister, Record, StableBTreeMap, Ok, None, Some, Err, Result, text,int, Variant, nat64, ic, Opt, Vec } from 'azle';
+import { query, update, Canister, Record, StableBTreeMap, Ok, None, Some, Err, Result, text, int, Variant, nat64, ic, Opt, Vec } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 // Define vehicle data structure
@@ -29,32 +29,47 @@ const vehiclesStorage = StableBTreeMap(text, Vehicle, 0);
 export default Canister({
   // Add a vehicle to vehiclesStorage
   addVehicle: update([VehicleData], Result(Vehicle, Error), (data) => {
-    const vehicle = { id: uuidv4(), createdAt: ic.time(), updatedAt: None, ...data };
-    vehiclesStorage.insert(vehicle.id, vehicle);
+    const vehicleId = uuidv4();
+    const vehicle = { id: vehicleId, ...data, createdAt: ic.time() };
+
+    if (vehiclesStorage.get(vehicleId)) {
+      return Err({ InvalidPayload: `Vehicle with id=${vehicleId} already exists` });
+    }
+
+    vehiclesStorage.insert(vehicleId, vehicle);
     return Ok(vehicle);
   }),
 
   // Get all vehicles from the storage
   getVehicles: query([], Result(Vec(Vehicle), Error), () => {
-    return Ok(vehiclesStorage.values());
+    const vehicles = vehiclesStorage.values();
+    if (!vehicles.length) {
+      return Err({ NotFound: `No vehicles found` });
+    }
+    return Ok(vehicles);
   }),
 
   // Get a specific vehicle from the storage using the id
   getVehicle: query([text], Result(Vehicle, Error), (id) => {
     const vehicleOpt = vehiclesStorage.get(id);
-    if ("None" in vehicleOpt) {
+    if (!vehicleOpt.isSome()) {
       return Err({ NotFound: `Vehicle with id=${id} not found` });
     }
-    return Ok(vehicleOpt.Some);
+    return Ok(vehicleOpt.get());
   }),
 
   // Update a vehicle already in the storage using the id
   updateVehicle: update([text, VehicleData], Result(Vehicle, Error), (id, data) => {
     const vehicleOpt = vehiclesStorage.get(id);
-    if ("None" in vehicleOpt) {
+    if (!vehicleOpt.isSome()) {
       return Err({ NotFound: `Couldn't update vehicle with id=${id}. Vehicle not found` });
     }
-    const vehicle = vehicleOpt.Some;
+
+    const vehicle = vehicleOpt.get();
+    if (vehicle.id !== id) {
+      return Err({ InvalidPayload: `Mismatching vehicle IDs` });
+    }
+
     const updatedVehicle = { ...vehicle, ...data, updatedAt: Some(ic.time()) };
     vehiclesStorage.insert(vehicle.id, updatedVehicle);
     return Ok(updatedVehicle);
@@ -63,25 +78,24 @@ export default Canister({
   // Delete a vehicle from the storage using the id
   deleteVehicle: update([text], Result(Vehicle, Error), (id) => {
     const deletedVehicle = vehiclesStorage.remove(id);
-    if ("None" in deletedVehicle) {
+    if (!deletedVehicle.isSome()) {
       return Err({ NotFound: `Couldn't delete vehicle with id=${id}. Vehicle not found` });
     }
-    return Ok(deletedVehicle.Some);
+    return Ok(deletedVehicle.get());
   })
 });
 
 // Existing globalThis.crypto code remains unchanged
 
-globalThis.crypto = { 
-    // @ts-ignore 
-    getRandomValues: () => { let array = new Uint8Array(32);
+globalThis.crypto = { 
+  // @ts-ignore 
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
 
     for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
+      array[i] = Math.floor(Math.random() * 256);
     }
 
     return array;
-}
-
+  }
 };
-
